02351f4bda843214509df82e97e3da837db61633
Tarun Gorai <goraitarunkumar@gmail.com>
2019-09-13 19:30:02 +0530

    set default value by one way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f', {static: true}) signUpForm: NgForm;
+  defaultQuestion = 'pet';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
